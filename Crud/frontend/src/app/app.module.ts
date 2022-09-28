0d82e56dd770a276383408fa8856b0896fa4a875
Alexandregv1080p <alexandregv1999@gmail.com>
2022-09-28 15:57:04 -0300

    Usando RouterLinks

--- Crud/frontend/src/app/app.module.ts
+++ Crud/frontend/src/app/app.module.ts
@@ -11,7 +11,12 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
+
 
 @NgModule({
   declarations: [
@@ -19,6 +24,8 @@ import {MatListModule} from '@angular/material/list';
     HeaderComponent,
     FooterComponent,
     NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +33,8 @@ import {MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
